1faec2fd3195da85d34a64b4706a6b2c163625ab
Ashok Dhungana <you@example.com>
2023-11-07 12:53:26 +0545

    Day3

---
+++ src/Main.java
@@ -0,0 +1,74 @@
+class  Cylinder
+{
+    private int radius;
+    private int height;
+
+    public Cylinder()
+    {
+        radius=height=1;
+    }
+    public Cylinder(int r,int h)
+    {
+        radius=r;
+        height=h;
+    }
+    public int getHeight()
+    {
+        return height;
+    }
+    public int getRadius()
+    {
+        return radius;
+    }
+
+    public void setHeight(int h)
+    {
+        if(h>=0)
+            height=h;
+        else
+            height=0;
+    }
+    public void setRadius(int r)
+    {
+        if(r>=0)
+            radius=r;
+        else
+            radius=0;
+    }
+
+    public double lidArea()
+    {
+        return Math.PI*radius*radius;
+    }
+    public double perimeter()
+    {
+        return 2*Math.PI*radius;
+    }
+    public double drumArea()
+    {
+        return 2*lidArea()+perimeter()*height;
+    }
+    public double volume()
+    {
+        return lidArea()*height;
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cylinder c = new Cylinder();
+        c.setHeight(10);
+        c.setRadius(7);
+
+
+        System.out.println("LidArea " + c.lidArea());
+        System.out.println("Circumference " + c.perimeter());
+        System.out.println("totalSurfaceArea " + c.drumArea());
+        System.out.println("Volume " + c.volume());
+        System.out.println("Height" + c.getHeight());
+        System.out.println("Radius" + c.getRadius());
+
+    }
+}
